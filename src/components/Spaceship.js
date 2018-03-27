e40b60e16b9fa6c9e91f98715033025d02a8dbbe
Vijay Raju <vijaypraju@gmail.com>
2018-03-27 15:37:03 -0700

    fixed HTML

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -4,10 +4,10 @@ export default class Spaceship extends React.Component {
   render() {
     return(
       <div>
-        <p>{this.props.name}</p>
-        <p>{this.props.speed}</p>
-        <p>{this.props.hasRockets}</p>
-        <p>{this.props.colors.join('')}</p>
+        <p>Name: {this.props.name}</p>
+        <p>Speed: {this.props.speed}</p>
+        <p>Rockets: {this.props.hasRockets}</p>
+        <p>Colors: {this.props.colors.join(' ')}</p>
       </div>
     )
   }
