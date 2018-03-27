00afd1e4b68d3a7e2adbb1db0b786f57893beef0
Vijay Raju <vijaypraju@gmail.com>
2018-03-27 15:32:39 -0700

    added name

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,1 +1,11 @@
-// Code The Spaceship Component Here
+import React from 'react';
+
+export default class Spaceship extends React.Component {
+  render() {
+    return(
+      <div>
+        <p>{this.props.name}</p>
+      </div>
+    )
+  }
+};
